636da3047871992632e6faba875695a3a02aafd6
Eedrah <eedrah@users.noreply.github.com>
2015-09-14 20:36:42 +1200

    finishing touches

--- entry.js
+++ entry.js
@@ -1,19 +1,17 @@
 var author = 'Jesse Eedrah';
 var site = 'http://eedrah.com/me';
-console.log('Created by ' + author +  ', visit me at ' + site);
-
-/* Explaination of exploit */
+console.log('Created by ' + author +  ', visit me at ' + site ' to see how the exploit works');
 
 // Initialize with a random key
 var key = Math.random().toString().substring(2);
 
-console.log(key);
-
+// I didn't go out and buy a CDN account, but if this was real I would convince my company to pay. We need our own so that we control the (audited) source code so it won't change in the future.
 var secretCompanyCDN = 'https://cdnjs.cloudflare.com/ajax/libs/jsSHA/2.0.1/sha.js';
 var secretCompanyApiKey = '8601044367533177';
-
 importExternalLibrary(secretCompanyCDN, secretCompanyApiKey);
+// There is no trickery in what was downloaded - I promise. The library wasn't touched. But please feel free to check yourself against the source code on github. Speaking of which, the explaination of this exploit is on my github.
 
+generateKey() // Run to display text
 function generateKey() {
 	var input = document.querySelector('#user-input').value;
 	var message;
